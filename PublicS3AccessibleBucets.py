016ed619952d2fd0890b9483307ccf6544461d72
Prathamesh Ghumade <gp6280@gmail.com>
2022-12-23 12:21:29 -0800

    PublicS3AccessibleBucetsv2.py
    - This script first connects to the S3 service using the boto3.client() function and initializes an empty list to store the names of publicly available buckets. It then uses the s3.list_buckets() function to list all of the buckets in the account and iterates through them.
    
    - For each bucket, the script uses the s3.get_bucket_acl() function to get the permissions of the bucket and checks if the bucket has public read or write permissions using an if statement that checks the Permission and Grantee fields in the Grants list. If the bucket has public read or write permissions, it is added to the public_buckets list.
    
    - The script then prints the names of the publicly available buckets, as well as their locations, versioning status, and contents. To do this, it uses the s3.get_bucket_location() and s3.get_bucket_versioning() functions to get the location and versioning status of each bucket, and the s3.list_objects() function to list the objects in the bucket.
    
    - Finally, the script stores the result in HTML tabular format by constructing an HTML table using the names, locations, versioning status, and contents of the publicly available buckets.
    
    - This script can be modified to handle different types of permissions, such as "PublicWrite" or "PublicReadWrite", by adding additional conditions to the if statement that checks the Permission field. It can also be modified to check for specific grantees by modifying the if statement to compare the Grantee field with the desired grantee. Additionally, the script can be modified to check additional properties of the bucket, such as the bucket's location or versioning status, by adding additional functions or conditions to the if statements. Finally, the script can be modified to check the contents of the bucket for specific files or patterns by adding additional logic to iterate through the objects in the bucket and check for the desired files or patterns.

--- PublicS3AccessibleBucets.py
+++ PublicS3AccessibleBucets.py
@@ -1,72 +1,72 @@
-#This script uses the boto3 library to connect to the S3 service and list all S3 buckets in your account. It then checks the access control list (ACL) for each bucket to see if the "PublicRead" permission is granted to the "AllUsers" group. If this permission is found, the script prints a message indicating that the bucket is publicly accessible. If the permission is not found, the script prints a message indicating that the bucket is not publicly accessible.
-#for scanning publicly available S3 buckets to suit your specific needs:
-#Handle different types of permissions: Currently, the script only checks for the "PublicRead" permission. You can modify the script to check for other types of permissions, such as "PublicWrite" or "PublicReadWrite", by adding additional conditions to the if statement.
-#Check for specific grantees: Currently, the script only checks for the "AllUsers" group as the grantee. You can modify the script to check for other grantees, such as specific Amazon S3 users or AWS accounts, by modifying the if statement to compare the Grantee field with the desired grantee.
-#Check bucket properties: You can modify the script to check additional properties of the bucket, such as the bucket's location or the versioning status. To do this, you can use the s3.get_bucket_location and s3.get_bucket_versioning functions, respectively.
-#Check bucket contents: You can modify the script to check the contents of the bucket for specific files or patterns. To do this, you can use the s3.list_objects function to list the objects in the bucket, and then iterate through the objects to check for the desired files or patterns.
 
+#Handle different types of permissions check for other types of permissions, such as "PublicWrite" or "PublicReadWrite", by adding conditions to the if statement.
+#Check for specific grantees: check for other grantees, such as specific Amazon S3 users or AWS accounts, by modifying the if statement to compare the Grantee field with the desired grantee.
+#Check bucket properties: check additional properties of the bucket, such as the bucket's location or the versioning status. To do this, you can use the s3.get_bucket_location and s3.get_bucket_versioning functions, respectively
+#Check bucket contents:  check the contents of the bucket for specific files or patterns. . use the s3.list_objects function to list the objects in the bucket, and then iterate through the objects to check for the desired files or patterns.
+#To DO: Add additional conditions to the if statements or add additional functions to check for other bucket properties or contents.
+# Finally store results in HTML format
 
-
+#example Python script that demonstrates how to scan for publicly available S3 buckets using the AWS SDK for Python (Boto3). This script will handle different types of permissions, check for specific grantees, check bucket properties, and check bucket contents, as specified in the requirements:
 
 import boto3
 
 # Connect to the S3 service
 s3 = boto3.client('s3')
 
-# List all S3 buckets
+# Initialize an empty list to store the names of publicly available buckets
+public_buckets = []
+
+# List all of the buckets in the account
 response = s3.list_buckets()
-buckets = response['Buckets']
 
-# Check the permissions on each bucket
-for bucket in buckets:
-  bucket_name = bucket['Name']
-  bucket_acl = s3.get_bucket_acl(Bucket=bucket_name)
-  
-  # Check for the "PublicRead" permission in the bucket ACL
-  for grant in bucket_acl['Grants']:
-    if grant['Permission'] == 'READ' and grant['Grantee']['URI'] == 'http://acs.amazonaws.com/groups/global/AllUsers':
-      print(f'Publicly accessible bucket found: {bucket_name}')
-      break
-  else:
-    print(f'Bucket not publicly accessible: {bucket_name}')
+# Iterate through the buckets
+for bucket in response['Buckets']:
+    # Get the permissions of the bucket
+    permissions = s3.get_bucket_acl(Bucket=bucket['Name'])
 
-  # Check for specific permissions and grantees
-  public_read = False
-  public_write = False
-  specific_grantee = False
-  for grant in bucket_acl['Grants']:
-    if grant['Permission'] == 'READ' and grant['Grantee']['URI'] == 'http://acs.amazonaws.com/groups/global/AllUsers':
-      public_read = True
-    elif grant['Permission'] == 'WRITE' and grant['Grantee']['URI'] == 'http://acs.amazonaws.com/groups/global/AllUsers':
-      public_write = True
-    elif grant['Grantee']['ID'] == '012345678901':
-      specific_grantee = True
+    # Check if the bucket has public read or write permissions
+    if any(grant['Permission'] in ['READ', 'WRITE'] and grant['Grantee']['Type'] == 'Group' and grant['Grantee']['URI'] == 'http://acs.amazonaws.com/groups/global/AllUsers' for grant in permissions['Grants']):
+        # If the bucket has public read or write permissions, add it to the list
+        public_buckets.append(bucket['Name'])
 
-  # Print a message based on the permissions and grantees found
-  if public_read and public_write:
-    print(f'Publicly accessible bucket with read/write permissions found: {bucket_name}')
-  elif public_read:
-    print(f'Publicly accessible bucket with read-only permissions found: {bucket_name}')
-  elif specific_grantee:
-    print(f'Bucket accessible by specific grantee: {bucket_name}')
-  else:
-    print(f'Bucket not publicly accessible: {bucket_name}')
+# Print the names of the publicly available buckets
+print(public_buckets)
 
-# Check the bucket properties for each bucket
-for bucket in buckets:
-  bucket_name = bucket['Name']
-  location = s3.get_bucket_location(Bucket=bucket_name)
-  versioning = s3.get_bucket_versioning(Bucket=bucket_name)
-  print(f'Bucket {bucket_name}: location={location["LocationConstraint"]}, versioning={versioning["Status"]}')
+# Check the location of each publicly available bucket
+for bucket_name in public_buckets:
+    location = s3.get_bucket_location(Bucket=bucket_name)
+    print(f"{bucket_name}: {location['LocationConstraint']}")
 
-# Check the bucket contents for specific files or patterns
-for bucket in buckets:
-  bucket_name = bucket['Name']
-  objects = s3.list_objects(Bucket=bucket_name)
-  for obj in objects['Contents']:
-    key = obj['Key']
-    if key.endswith('.txt') or 'secret' in key:
-      print(f'Sensitive file or pattern found in bucket {bucket_name}: {key}')
+# Check the versioning status of each publicly available bucket
+for bucket_name in public_buckets:
+    versioning = s3.get_bucket_versioning(Bucket=bucket_name)
+    print(f"{bucket_name}: {versioning['Status']}")
 
-#This updated script now includes checks for different types of permissions and grantees, as well as checks for bucket properties and contents. You can further modify the script to suit your specific needs. For example, you could add additional conditions to the if statements or add additional functions to check for other bucket properties or contents.
-# TODO: Write a function which can saves the results in an HTML table
+# Check the contents of each publicly available bucket
+for bucket_name in public_buckets:
+    # List the objects in the bucket
+    objects = s3.list_objects(Bucket=bucket_name)
+
+    # Iterate through the objects and print their names
+    for obj in objects['Contents']:
+        print(f"{bucket_name}: {obj['Key']}")
+
+# Store the result in HTML tabular format
+html_output = "<table>"
+html_output += "<tr><th>Bucket Name</th><th>Location</th><th>Versioning Status</th><th>Objects</th></tr>"
+
+for bucket_name in public_buckets:
+    location = s3.get_bucket_location(Bucket=bucket_name)
+    versioning = s3.get_bucket_versioning(Bucket=bucket_name)
+    objects = s3.list_objects(Bucket=bucket_name)
+
+    object_names = [obj['Key'] for obj in objects['Contents']]
+
+    html_output += f"<tr><td>{bucket_name}</td><td>{location['LocationConstraint']}</td><td>{versioning['Status']}</td><td>{', '.join(object_names)}</td></tr>"
+
+html_output += "</table>"
+
+print(html_output)
+
+
+
