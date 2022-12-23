11b3622b23a65d565af231133243bc6119c8620f
Prathamesh Ghumade <gp6280@gmail.com>
2022-12-22 21:42:15 -0800

    Create SimpleSSTIDetector.py

---
+++ SimpleSSTIDetector.py
@@ -0,0 +1,37 @@
+#This code checks for common template tags and payloads that may indicate an SSTI vulnerability. 
+#It uses regular expressions to search for the template tags and simple string comparisons to check for the payloads.
+#Keep in mind that this is a very basic SSTI detector and may not detect all possible SSTI vulnerabilities. 
+#It is important to thoroughly test and validate user input to prevent SSTI vulnerabilities in your application.
+
+import re
+
+def detect_ssti(input_string):
+    # Check for Jinja2 template tags
+    if "{{" in input_string or "}}" in input_string:
+        return True
+
+    # Check for Django template tags
+    if "{%" in input_string or "%}" in input_string:
+        return True
+
+    # Check for Twig template tags
+    if "{{" in input_string or "}}" in input_string:
+        return True
+
+    # Check for PHP template tags
+    if "<?=" in input_string or "?>" in input_string:
+        return True
+
+    # Check for common SSTI payloads
+    payloads = [
+        "{{ 7*7 }}",
+        "{% if True %}True{% endif %}",
+        "{{ ''.__class__.__mro__[2].__subclasses__() }}",
+        "<?=system('id')?>"
+    ]
+    for payload in payloads:
+        if payload in input_string:
+            return True
+
+    # No SSTI detected
+    return False
